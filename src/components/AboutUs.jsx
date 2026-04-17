8b94936af6117be7b9a574347f359cb9ac611fd8
vaishalivaish11 <vaishali.vaish11@gmail.com>
2026-04-17 15:48:59 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div style={{ padding: "20px", color: "#fff" }}>
+      <h1>Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is dedicated to bringing nature closer to people.
+        We offer a wide variety of indoor and outdoor plants that help
+        create a greener and healthier environment.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
